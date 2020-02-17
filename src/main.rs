ee357a6ea478ca4040913e6141f6d0e150f0b2e1
Jake Carrington <jake.ethan.carrington@gmail.com>
2020-02-17 22:38:36 +0000

    cradle complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::io;
 use std::io::Read;
 
@@ -17,21 +18,66 @@ impl Cradle {
         let c: char = temp_buf.chars().collect::<Vec<char>>()[0];
         Ok(c)
     }
+
     fn get_char(&mut self) -> io::Result<()> {
         self.lookahead = self.read().unwrap();
         Ok(())
     }
+
     fn error(&self, message: String) {
         println!("\nError: {}", message);
     }
+
     fn abort(&self, message: String) {
         self.error(message);
         panic!("^^^^^^^");
     }
+
     fn expected(&self, expected: String) {
         self.abort(format!("{} expected", expected));
     }
-    fn match_char(&mut self, x: &char) {}
+
+    fn match_char(&mut self, x: &char) -> io::Result<()> {
+        if self.lookahead == *x {
+            self.get_char()?;
+        } else {
+            self.expected(format!("\"{}\"", x));
+        }
+
+        Ok(())
+    }
+
+    fn is_alpha(&self, x: &char) -> bool {
+        x.is_alphabetic()
+    }
+
+    fn is_digit(&self, x: &char) -> bool {
+        x.is_digit(10)
+    }
+
+    fn get_name(&self) -> char {
+        if !self.is_alpha(&self.lookahead) {
+            self.expected(String::from("Name"));
+        }
+
+        self.lookahead
+    }
+
+    fn get_num(&self) -> char {
+        if !self.is_digit(&self.lookahead) {
+            self.expected(String::from("Integer"));
+        }
+
+        self.lookahead
+    }
+
+    fn emit(&self, s: String) {
+        print!("{}", s);
+    }
+
+    fn emit_line(&self, s: String) {
+        println!("{}", s);
+    }
 }
 
 fn main() -> io::Result<()> {
