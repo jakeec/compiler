f88b38787c5160a67db9c3241be2996a8756387b
jakeec <jake.ethan.carrington@gmail.com>
2020-02-19 12:20:10 +0000

    added basic tests

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,131 @@
 use regex::Regex;
 use std::io;
 use std::io::Read;
+use std::string::String;
+
+enum ReaderArg {
+    Raw(String),
+    FilePath(String),
+    None,
+}
+
+struct StdinReader {
+    buffer: Vec<char>,
+}
+
+struct TestReader {
+    buffer: Vec<char>,
+}
+
+impl StdinReader {
+    fn new() -> Self {
+        Self { buffer: Vec::new() }
+    }
+}
+
+impl TestReader {
+    fn new() -> Self {
+        Self { buffer: Vec::new() }
+    }
+}
+
+trait Reader {
+    fn read(&mut self, arg: ReaderArg) -> io::Result<()>;
+    fn get_buffer(&self) -> Vec<char>;
+}
+
+impl Reader for StdinReader {
+    fn read(&mut self, arg: ReaderArg) -> io::Result<()> {
+        let mut temp_buf: String = String::new();
+        std::io::stdin().read_line(&mut temp_buf)?;
+        self.buffer = temp_buf
+            .chars()
+            .filter(|c| ![&'\n', &'\r'].contains(&c))
+            .collect::<Vec<char>>();
+
+        Ok(())
+    }
+
+    fn get_buffer(&self) -> Vec<char> {
+        self.buffer.clone()
+    }
+}
+
+impl Reader for TestReader {
+    fn read(&mut self, arg: ReaderArg) -> io::Result<()> {
+        match arg {
+            ReaderArg::Raw(s) => {
+                self.buffer = s
+                    .chars()
+                    .filter(|c| ![&'\n', &'\r'].contains(&c))
+                    .collect::<Vec<char>>();
+            }
+            _ => panic!("Invalid argument!"),
+        }
+
+        Ok(())
+    }
+
+    fn get_buffer(&self) -> Vec<char> {
+        self.buffer.clone()
+    }
+}
+
+struct StdoutWriter {}
+struct TestWriter {
+    output: String,
+}
+
+impl TestWriter {
+    fn new() -> Self {
+        Self {
+            output: String::new(),
+        }
+    }
+}
+
+trait Writer {
+    fn write(&mut self, output: String) {}
+    fn writeln(&mut self, output: String) {}
+}
+
+impl Writer for StdoutWriter {
+    fn write(&mut self, output: String) {
+        print!("{}", output);
+    }
+
+    fn writeln(&mut self, output: String) {
+        println!("{}", output);
+    }
+}
+
+impl Writer for TestWriter {
+    fn write(&mut self, output: String) {
+        self.output = format!("{}{}", self.output, output);
+    }
+
+    fn writeln(&mut self, output: String) {
+        self.output = format!("{}\n{}", self.output, output);
+    }
+}
 
 #[derive(Debug)]
-struct Cradle {
+struct Cradle<'a, R: Reader, W: Writer> {
     lookahead: Option<char>,
     source: Vec<char>,
     counter: usize,
+    reader: R,
+    writer: Box<&'a mut W>,
 }
 
-impl Cradle {
-    fn new() -> Self {
+impl<'a, R: Reader, W: Writer> Cradle<'a, R, W> {
+    fn new(reader: R, writer: &'a mut W) -> Self {
         Self {
             lookahead: None,
             source: Vec::new(),
             counter: 0,
+            reader: reader,
+            writer: Box::new(writer),
         }
     }
 
@@ -24,12 +135,7 @@ impl Cradle {
     }
 
     fn read(&mut self) -> io::Result<()> {
-        let mut temp_buf: String = String::new();
-        std::io::stdin().read_line(&mut temp_buf)?;
-        self.source = temp_buf
-            .chars()
-            .filter(|c| ![&'\n', &'\r'].contains(&c))
-            .collect::<Vec<char>>();
+        self.source = self.reader.get_buffer();
         Ok(())
     }
 
@@ -46,17 +152,25 @@ impl Cradle {
         Ok(())
     }
 
-    fn error(&self, message: String) {
-        println!("\nError: {}", message);
+    fn error(&mut self, message: String) {
+        self.writer.write(format!("\nError: {}", message));
     }
 
-    fn abort(&self, message: String) {
+    fn abort(&mut self, message: String) {
         self.error(message);
         panic!("^^^^^^^");
     }
 
-    fn expected(&self, expected: String) {
+    fn expected(&mut self, expected: String) {
         self.abort(format!("{} expected", expected));
+    }
+
+    fn emit(&mut self, s: String) {
+        self.writer.write(format!("{}", s));
+    }
+
+    fn emit_line(&mut self, s: String) {
+        self.writer.writeln(format!("{}", s));
     }
 
     fn match_char(&mut self, x: &char) -> io::Result<()> {
@@ -97,14 +211,6 @@ impl Cradle {
         num.unwrap()
     }
 
-    fn emit(&self, s: String) {
-        print!("{}", s);
-    }
-
-    fn emit_line(&mut self, s: String) {
-        println!("{}", s);
-    }
-
     fn term(&mut self) {
         let num = self.get_num();
         self.emit_line(format!("MOVE #{},D0", num));
@@ -142,8 +248,77 @@ impl Cradle {
 }
 
 fn main() -> io::Result<()> {
-    let mut cradle = Cradle::new();
+    let mut reader = StdinReader::new();
+    reader.read(ReaderArg::None);
+    let mut writer = StdoutWriter {};
+    let mut cradle = Cradle::new(reader, &mut writer);
     cradle.init();
     cradle.expression();
     Ok(())
+}
+
+#[cfg(test)]
+mod CradleTests {
+    use super::*;
+
+    #[test]
+    fn given_single_term_expression_output_move_instruction() {
+        let mut reader = TestReader::new();
+        reader.read(ReaderArg::Raw(String::from("1")));
+        let mut writer = TestWriter::new();
+        let mut cradle = Cradle::new(reader, &mut writer);
+        cradle.init();
+
+        cradle.expression();
+
+        assert_eq!(String::from("\nMOVE #1,D0\nMOVE D0,D1"), writer.output);
+    }
+
+    #[test]
+    fn given_add_operation_output_add_instructions() {
+        let mut reader = TestReader::new();
+        reader.read(ReaderArg::Raw(String::from("1+2")));
+        let mut writer = TestWriter::new();
+        let mut cradle = Cradle::new(reader, &mut writer);
+        cradle.init();
+
+        cradle.expression();
+
+        assert_eq!(
+            String::from("\nMOVE #1,D0\nMOVE D0,D1\nMOVE #2,D0\nADD D1,D0"),
+            writer.output
+        );
+    }
+
+    #[test]
+    fn given_subtract_operation_output_subtract_instructions() {
+        let mut reader = TestReader::new();
+        reader.read(ReaderArg::Raw(String::from("1-2")));
+        let mut writer = TestWriter::new();
+        let mut cradle = Cradle::new(reader, &mut writer);
+        cradle.init();
+
+        cradle.expression();
+
+        assert_eq!(
+            String::from("\nMOVE #1,D0\nMOVE D0,D1\nMOVE #2,D0\nSUB D1,D0\nNEG D1"),
+            writer.output
+        );
+    }
+
+    #[test]
+    fn given_multiple_operators_output_correct_assembly() {
+        let mut reader = TestReader::new();
+        reader.read(ReaderArg::Raw(String::from("1-2+3-4+7")));
+        let mut writer = TestWriter::new();
+        let mut cradle = Cradle::new(reader, &mut writer);
+        cradle.init();
+
+        cradle.expression();
+
+        assert_eq!(
+            String::from("\nMOVE #1,D0\nMOVE D0,D1\nMOVE #2,D0\nSUB D1,D0\nNEG D1\nMOVE #3,D0\nADD D1,D0\nMOVE #4,D0\nSUB D1,D0\nNEG D1\nMOVE #7,D0\nADD D1,D0"),
+            writer.output
+        );
+    }
 }
